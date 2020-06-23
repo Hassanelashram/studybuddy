5bfc1c29092fb103c6ed93a12861ea25a4b984d0
Hassanelashram <elashramhassan@gmail.com>
2020-06-23 12:12:28 +0200

    did profile and search functiion, seed was also improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
-
   def index
+    query = params[:query]
+    if query.present?
+      @students = User.joins(learning_subjects: :subject).where("subjects.name ILIKE ?", query)
+      @teachers = User.joins(teaching_subjects: :subject).where("subjects.name ILIKE ?", query)
+    else
+      @user = User.all
+    end
   end
 
+  def profile
+    @user = User.find(params[:id])
+  end
 end
