615f05d5866de00f582c2fe77629dc75a8635d64
Hassanelashram <elashramhassan@gmail.com>
2020-06-24 16:48:17 +0200

    pundit basics added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,6 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_chat
 
-  def index
-    @other_user = current_user == @chat.sender ? @chat.recipient : @chat.sender
-    @messages = @chat.messages.order(created_at: :asc).last(20)
-    @message = Message.new
-  end
-
   def create
     @message = @chat.messages.new(message_params)
     @message.user = current_user
