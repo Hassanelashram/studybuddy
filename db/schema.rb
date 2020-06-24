35d67d59cc0578f4a79691875da7e93c10ed2b67
Hassanelashram <elashramhassan@gmail.com>
2020-06-24 14:30:12 +0200

    function of chat done but needs pundit and much more

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_104901) do
+ActiveRecord::Schema.define(version: 2020_06_24_094957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_06_23_104901) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "chats", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "learning_subjects", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "subject_id", null: false
@@ -43,6 +50,16 @@ ActiveRecord::Schema.define(version: 2020_06_23_104901) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["subject_id"], name: "index_learning_subjects_on_subject_id"
     t.index ["user_id"], name: "index_learning_subjects_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -80,6 +97,8 @@ ActiveRecord::Schema.define(version: 2020_06_23_104901) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "learning_subjects", "subjects"
   add_foreign_key "learning_subjects", "users"
+  add_foreign_key "messages", "chats"
+  add_foreign_key "messages", "users"
   add_foreign_key "teaching_subjects", "subjects"
   add_foreign_key "teaching_subjects", "users"
 end
