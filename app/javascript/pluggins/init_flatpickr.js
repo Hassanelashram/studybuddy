478da68420d9be734806b72441c76f1d2aea6ac4
Kasimir von Halem <kasimir.halem@me.com>
2020-06-25 15:27:38 +0200

    finished the booking

---
+++ app/javascript/pluggins/init_flatpickr.js
@@ -0,0 +1,14 @@
+import flatpickr from "flatpickr";
+
+const schedual = () => {
+  const inputs = document.querySelectorAll(".kasi")
+  console.log(inputs)
+  inputs.forEach((input) => {
+    flatpickr(input, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+    })
+  })
+}
+
+export {schedual}
