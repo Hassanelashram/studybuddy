b6394c2f2f84d5eea13680563b8bf1fad2cdb987
Xavier Everaert <xeveraert@gmail.com>
2020-06-25 10:47:36 +0200

    halfway there

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_094957) do
+ActiveRecord::Schema.define(version: 2020_06_24_130525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_094957) do
     t.text "bio"
     t.string "location"
     t.integer "price"
+    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
