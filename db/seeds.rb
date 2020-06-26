fcb6427258fd1cfe04511f943cd4a25dd14f9a4a
Kasimir von Halem <kasimir.halem@me.com>
2020-06-26 14:58:47 +0200

    languag seeds are done and the footer is fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,49 +4,51 @@ User.destroy_all
 
 subjects = ['Programming', 'Maths', 'Physics', 'History', 'Geography', 'Spanish']
 
+@languages = ['French', 'German', 'Spanish']
+
 subjects.each do |s|
   Subject.create!(name: s)
 end
 
 
-2.times do
+20.times do
  bio = "#{Faker::Job.title}\n #{Faker::University.name}\n #{Faker::TvShows::SiliconValley.quote}"
  user = User.create!(
     name: Faker::Games::Pokemon.name,
     surname: Faker::Games::Pokemon.name,
-    location: Faker::Games::Pokemon.location,
     email: Faker::Internet.email,
     bio: bio,
     location: Faker::Address.state,
-    password: '123456')
+    password: '123456',
+    language: @languages.sample)
  Subject.all.sample(2).each { |s| user.learning_subjects.create(subject: s)}
 end
 
-2.times do
+20.times do
  bio = "#{Faker::Job.title}\n #{Faker::University.name}\n #{Faker::TvShows::SiliconValley.quote}"
  user = User.create!(
     name: Faker::Games::Pokemon.name,
     surname: Faker::Games::Pokemon.name,
-    location: Faker::Games::Pokemon.location,
     email: Faker::Internet.email,
     password: '123456',
     price: rand(5...15),
     bio: bio,
-    location: Faker::Address.state)
+    location: Faker::Address.state,
+    language: @languages.sample)
     Subject.all.sample(2).each { |s| user.teaching_subjects.create(subject: s)}
 end
 
-2.times do
+20.times do
  bio = "#{Faker::Job.title}\n #{Faker::University.name}\n #{Faker::TvShows::SiliconValley.quote}"
  user = User.create!(
     name: Faker::Games::Pokemon.name,
     surname: Faker::Games::Pokemon.name,
-    location: Faker::Games::Pokemon.location,
     email: Faker::Internet.email,
     password: '123456',
     price: rand(5...15),
     bio: bio,
-    location: Faker::Address.state)
+    location: Faker::Address.state,
+    language: @languages.sample)
  Subject.all.sample(2).each { |s| user.learning_subjects.create(subject: s)}
  Subject.all.sample(2).each { |s| user.teaching_subjects.create(subject: s)}
 end
