edd4bbad0357f04823d86a7dbb6bfd45169f2c74
Hassanelashram <elashramhassan@gmail.com>
2020-06-26 13:14:33 +0200

    imporved

--- app/javascript/pluggins/init_select2.js
+++ app/javascript/pluggins/init_select2.js
@@ -2,8 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
-  console.log('sup')
+  $('.select2').select2({width: '100%'});
 };
 
 export { initSelect2 };
